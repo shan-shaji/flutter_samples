89ea8cf90fc81d215c844dcd9dc399a4d6bb5c30
Aljo Biju <64011196+Aljobiju@users.noreply.github.com>
2020-10-18 21:32:23 +0530

    Update colors.dart
    added new color constant

--- profile_page/lib/constants/colors.dart
+++ profile_page/lib/constants/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color profileContainerColor = Color(0xffFC7A6C);
 Color profileContainerColor2 = Color(0xffFA6E85);
 Color profileContainerColor3 = Color(0xffFA6E85);
+Color profileContainerColor4 = Color(0xffFA6E85);
