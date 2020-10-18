c02f008c68d407d3f294fd67dabf46187449aa0d
Aljo Biju <64011196+Aljobiju@users.noreply.github.com>
2020-10-18 21:30:26 +0530

    Update colors.dart

--- profile_page/lib/constants/colors.dart
+++ profile_page/lib/constants/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 Color profileContainerColor = Color(0xffFC7A6C);
 Color profileContainerColor2 = Color(0xffFA6E85);
+Color profileContainerColor3 = Color(0xffFA6E85);
